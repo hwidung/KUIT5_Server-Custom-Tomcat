db6b0904b5fa5d80d8cc1b9ad9af94cb2b6e0b3f
hwidung <hwhwhw74@naver.com>
2025-04-02 05:12:53 +0900

    feat: get 방식으로 회원가입 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,14 +1,19 @@
 package webserver;
 
+import db.MemoryUserRepository;
+import http.util.HttpRequestUtils;
+import model.User;
+
 import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.nio.file.Files;
 
 public class RequestHandler implements Runnable {
-    Socket connection;
     private static final Logger log = Logger.getLogger(RequestHandler.class.getName());
+    private Socket connection;
 
     public RequestHandler(Socket connection) {
         this.connection = connection;
@@ -31,61 +36,86 @@ public class RequestHandler implements Runnable {
             String[] tokens = requestLine.split(" ");
             String uri = tokens[1];
 
-            // 기본 경로 설정
-            if (uri.equals("/")) {
-                uri = "/index.html";
-            }
-
-            // 요청한 파일 경로 설정
-            String filePath = "./webapp" + uri;
-            File file = new File(filePath);
-
-            if (file.exists()) {
-                byte[] body = Files.readAllBytes(file.toPath());
-                response200Header(dos, body.length);
-                responseBody(dos, body);
+            if (uri.equals("/user/form.html")) {
+                handleFormRequest(dos);
+            } else if (uri.startsWith("/user/signup")) {
+                handleUserCreate(uri, dos);
             } else {
-                // 404 Not Found 응답
-                response404Header(dos);
-            }
+                // 기본 경로 설정
+                if (uri.equals("/")) {
+                    uri = "/index.html";
+                }
+                // 요청한 파일 경로 설정
+                String filePath = "./webapp" + uri;
+                File file = new File(filePath);
 
-            byte[] body = "Hello World".getBytes();
+                if (file.exists()) {
+                    byte[] body = Files.readAllBytes(file.toPath());
+                    response200Header(dos, body.length);
+                    responseBody(dos, body);
+                } else {
+                    // 404 Not Found 응답
+                    response404Header(dos);
+                }
+            }
+        } catch (IOException e) {
+            log.log(Level.SEVERE, e.getMessage());
+        }
+    }
+
+    private void handleFormRequest(DataOutputStream dos) throws IOException {
+        String filePath = "./webapp/user/form.html";
+        File file = new File(filePath);
+
+        if (file.exists()) {
+            byte[] body = Files.readAllBytes(file.toPath());
             response200Header(dos, body.length);
             responseBody(dos, body);
-
-        } catch (IOException e) {
-            log.log(Level.SEVERE, e.getMessage());
+        } else {
+            response404Header(dos);
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            log.log(Level.SEVERE, e.getMessage());
-        }
+    private void handleUserCreate(String uri, DataOutputStream dos) throws IOException {
+        // 쿼리스트링 파싱
+        String queryString = uri.split("\\?")[1];
+        Map<String, String> params = HttpRequestUtils.parseQueryParameter(queryString);
+
+        // User 객체 생성 및 저장
+        String userId = params.get("userId");
+        String password = params.get("password");
+        String name = params.get("name");
+        String email = params.get("email");
+
+        User user = new User(userId, password, name, email);
+        MemoryUserRepository.getInstance().addUser(user);
+
+        // 302 리다이렉트 응답
+        response302Header(dos, "/index.html");
     }
 
-    private void response404Header(DataOutputStream dos) {
-        try {
-            dos.writeBytes("HTTP/1.1 404 Not Found \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            log.log(Level.SEVERE, e.getMessage());
-        }
+    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) throws IOException {
+        dos.writeBytes("HTTP/1.1 200 OK \r\n");
+        dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+        dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+        dos.writeBytes("\r\n");
     }
 
-    private void responseBody(DataOutputStream dos, byte[] body) {
-        try {
-            dos.write(body, 0, body.length);
-            dos.flush();
-        } catch (IOException e) {
-            log.log(Level.SEVERE, e.getMessage());
-        }
+    private void response302Header(DataOutputStream dos, String location) throws IOException {
+        dos.writeBytes("HTTP/1.1 302 Found \r\n");
+        dos.writeBytes("Location: " + location + "\r\n");
+        dos.writeBytes("\r\n");
     }
 
+    private void response404Header(DataOutputStream dos) throws IOException {
+        dos.writeBytes("HTTP/1.1 404 Not Found \r\n");
+        dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+        dos.writeBytes("\r\n");
+        dos.writeBytes("<h1>404 Not Found</h1>");
+    }
+
+    private void responseBody(DataOutputStream dos, byte[] body) throws IOException {
+        dos.write(body, 0, body.length);
+        dos.flush();
+    }
 }
